724c8d531c06cbbe8fc23f645341e99fae9fec5b
J Brennan <J_Brennan@me.com>
2021-04-12 21:14:51 -0400

    readme

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
     const categoryData = await Category.findAll({
       include: [{ model: Product }]
     })
-    res.status(200).json(driverData);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
